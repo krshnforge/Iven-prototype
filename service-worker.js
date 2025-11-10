56eac8495fc5276d09f279561821e759722d3bfb
krshnforge <krshnforge@proton.me>
2025-11-11 02:58:25 +0530

    Add service worker for caching files

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "iven-hub-v1";
+const FILES_TO_CACHE = [
+  "/Iven-prototype/",
+  "/Iven-prototype/index.html",
+  "/Iven-prototype/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
